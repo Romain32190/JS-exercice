6e3be1331bc43110c3903866d35ecd239175db8f
Romain32190 <romain.lafargue36@gmail.com>
2017-03-27 10:54:46 +0200

    Exercice 3

--- array/app.js
+++ array/app.js
@@ -1,5 +1,10 @@
 var mois = ['Janvier','Février','Mars','Avril', 'Mai','Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Decembre'];
 
+//Exercice 1//
 console.log(mois);
 
+//Exercice 2//
 console.log(mois[2]);
+
+//Exercice 3//
+console.log(mois[4]);
