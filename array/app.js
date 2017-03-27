63af5c69ea1513bd6f93f979dd600b3a7dff7c47
Romain32190 <romain.lafargue36@gmail.com>
2017-03-27 13:54:20 +0200

    Exercice 11

--- array/app.js
+++ array/app.js
@@ -1,39 +1,41 @@
+	var fruits = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
 
-var fruits = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
 
+	var mois = ['Janvier','Février','Mars','Avril', 'Mai','Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Decembre'];
 
-var mois = ['Janvier','Février','Mars','Avril', 'Mai','Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Decembre'];
+	//Exercice 1//
+	console.log(mois);
 
-//Exercice 1//
-console.log(mois);
+	//Exercice 2//
+	console.log(mois[2]);
 
-//Exercice 2//
-console.log(mois[2]);
+	//Exercice 3//
+	console.log(mois[4]);
 
-//Exercice 3//
-console.log(mois[4]);
+	//Exercice 4//
+	console.log(mois[10]);
 
-//Exercice 4//
-console.log(mois[10]);
+	//Exercice 5//
+	mois.splice(7, 1, "Août");
+	console.log(mois);
 
-//Exercice 5//
-mois.splice(7, 1, "Août");
-console.log(mois);
+	//Exercice 6//
+	for(var i = 0; i<=mois.length; i++){
+	console.log(mois);
+	}
+	//Exercice 8//
 
-//Exercice 6//
-for(var i = 0; i<=mois.length; i++){
-console.log(mois);
-}
+	fruits.push("Courgette");
+	console.log(fruits);
 
-//Exercice 8//
+	//Exercice 9//
+	fruits[4].push('Citron');
+	console.log(fruits);
 
-fruits.push("Courgette");
-console.log(fruits);
+	//Exercice 10//
+	fruits.splice(1, 1);
+	console.log(fruits);
 
-//Exercice 9//
-fruits.splice(4, 0, "Citron");
-console.log(fruits);
-
-//Exercice 10//
-fruits.splice(1, 1);
-console.log(fruits);
+	//Exercice 11//
+	fruits.splice(2, 0, "Poire");
+	console.log(fruits);
