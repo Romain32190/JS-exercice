9fc95839d88b5b75d5e58fb4d446f1225c550f6e
Romain32190 <romain.lafargue36@gmail.com>
2017-03-27 16:52:43 +0200

    Exercice 5

--- functions/app.js
+++ functions/app.js
@@ -1,25 +1,49 @@
-//Exercice 1//
-function essai(){
+	//Exercice 1//
+	function essai(){
 
-	return true;
+		return true;
+	}
+
+	console.log( essai() );
+
+	//Exercice 2//
+
+	function string(a){
+		return a;
+	}
+
+	console.log(string("Hello world!"));
+
+	//Exercice 3//
+
+	function Romain(a,b){
+		return a+b;
+	}
+
+	console.log(Romain("Hello world!","Salut!"));
+
+	//Exercice 4//
+
+	function nombres (chiffre1, chiffre2){
+
+		if (chiffre1 > chiffre2){
+			return "le premier nombre est plus grand";
+		}
+
+		else if (chiffre1 < chiffre2){
+            return "le nombre est plus petit";
+		}
+
+		else{
+            return "Ils sont égaux";
+		}
 }
 
-console.log( essai() );
+	console.log(nombres(10,8));
 
-//Exercice 2//
+function Nombrechaine(nombre, chaine){
+	return nombre+ " " + chaine;
 
-function string(a){
-	return a;
 }
 
-console.log(string("Hello world!"));
-
-//Exercice 3//
-
-function Romain(a,b){
-	return a+b;
-}
-
-console.log(Romain("Hello world!","Salut!"));
-
-//Exercice 4//
+console.log(Nombrechaine(19, "Romain"))
